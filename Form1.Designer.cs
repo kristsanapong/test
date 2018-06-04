2137f2ff34c92240ab22c08611c583897b260842
Kritsanapong Poomdokmai <kristsanapong_p@cmu.ac.th>
2018-06-04 21:17:55 +0700

    Assignment1 Internship

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace test
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.txtNum = new System.Windows.Forms.TextBox();
+			this.txtBox = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.treeView1 = new System.Windows.Forms.TreeView();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 38);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(115, 33);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Write";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// txtNum
+			// 
+			this.txtNum.Location = new System.Drawing.Point(12, 12);
+			this.txtNum.Name = "txtNum";
+			this.txtNum.Size = new System.Drawing.Size(115, 20);
+			this.txtNum.TabIndex = 1;
+			this.txtNum.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+			// 
+			// txtBox
+			// 
+			this.txtBox.Location = new System.Drawing.Point(133, 11);
+			this.txtBox.Multiline = true;
+			this.txtBox.Name = "txtBox";
+			this.txtBox.Size = new System.Drawing.Size(301, 250);
+			this.txtBox.TabIndex = 2;
+			this.txtBox.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(458, 50);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(115, 33);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "Files";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(458, 11);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(115, 33);
+			this.button3.TabIndex = 4;
+			this.button3.Text = "Folder";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// treeView1
+			// 
+			this.treeView1.Location = new System.Drawing.Point(133, 284);
+			this.treeView1.Name = "treeView1";
+			this.treeView1.Size = new System.Drawing.Size(301, 249);
+			this.treeView1.TabIndex = 5;
+			this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.Location = new System.Drawing.Point(579, 11);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(301, 238);
+			this.listBox1.TabIndex = 6;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(892, 534);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.treeView1);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.txtBox);
+			this.Controls.Add(this.txtNum);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Test";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox txtNum;
+		private System.Windows.Forms.TextBox txtBox;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.TreeView treeView1;
+		private System.Windows.Forms.ListBox listBox1;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace test
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			string CurDir = Directory.GetCurrentDirectory();
+			CurDir = "..\\..";
+			DirectoryInfo directortInfo = new DirectoryInfo(CurDir);
+			if (directortInfo.Exists) {
+				treeView1.AfterSelect += treeView1_AfterSelect;
+				BuildTree(directortInfo, treeView1.Nodes);
+			}
+		}
+
+		private void BuildTree(DirectoryInfo directoryInfo, TreeNodeCollection addInMe)
+		{
+			TreeNode curNode = addInMe.Add(directoryInfo.Name);
+
+			foreach (FileInfo file in directoryInfo.GetFiles())
+			{
+				curNode.Nodes.Add(file.FullName, file.Name);
+			}
+			foreach (DirectoryInfo subdir in directoryInfo.GetDirectories())
+			{
+				BuildTree(subdir, curNode.Nodes);
+			}
+		}
+
+		/*private TreeListNode FindTreeNode(TreeListNode node, Enumerations.ItemType type, Nullable<long> id)
+		{
+			foreach (TreeListNode child in node.Nodes)
+			{
+				if ((Enumerations.ItemType)child[2] == type &&
+					(id == null || (long)child[0] == id.Value))
+				{
+
+					return child;
+				}
+
+				if (child.HasChildren)
+				{
+					TreeListNode found = FindTreeNode(child, type, id);
+					if (found != null)
+					{
+						return found;
+					}
+				}
+			}
+			return null;
+		}
+		*/
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			//Write
+			string txt="hello world\r\n";
+			string ans = txt;
+			string txtNum = this.txtNum.Text;
+			this.Text = txtNum;
+			int num = int.Parse(this.txtNum.Text);
+			for (int i = 1; i < num; i++)
+			{
+				ans = ans + txt;
+				this.txtBox.Text = i.ToString();
+			}
+			File.WriteAllText("Files.txt", ans);
+			//File.WriteAllText("Files.txt",txt);
+
+
+			//Read
+			string contents = File.ReadAllText("Files.txt");
+			this.txtBox.Text = contents;
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void textBox2_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			string CurDir = Directory.GetCurrentDirectory();
+			CurDir = "..\\..";
+
+			string[] curFolder = Directory.GetDirectories(CurDir);
+			foreach (string folder in curFolder)
+			{
+				listBox1.Items.Add(folder);
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			string CurDir = Directory.GetCurrentDirectory();
+			CurDir = "..\\..";
+			string[] curFiles = Directory.GetFiles(CurDir);
+			foreach (string file in curFiles)
+			{
+				listBox1.Items.Add(file);
+			}
+		}
+		
+		private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+		{
+			
+		}
+
+		private void richTextBox1_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
