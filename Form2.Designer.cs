c6d053163e08b209ce9b201dc50db1b53cf92726
Kritsanapong Poomdokmai <kristsanapong_p@cmu.ac.th>
2018-06-05 22:36:19 +0700

    Assginment 2

---
+++ Form2.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace test
+{
+	partial class Form2
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.txtNum1 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.SystemColors.Control;
+			this.panel1.Location = new System.Drawing.Point(363, 12);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(425, 398);
+			this.panel1.TabIndex = 0;
+			this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(104, 81);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(172, 29);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Create Pixel";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(104, 160);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(172, 68);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "Save in Binary File";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(104, 268);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(172, 68);
+			this.button3.TabIndex = 3;
+			this.button3.Text = "Read from Binary File";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// txtNum1
+			// 
+			this.txtNum1.Location = new System.Drawing.Point(104, 55);
+			this.txtNum1.Name = "txtNum1";
+			this.txtNum1.Size = new System.Drawing.Size(172, 20);
+			this.txtNum1.TabIndex = 4;
+			// 
+			// Form2
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.txtNum1);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.panel1);
+			this.Name = "Form2";
+			this.Text = "Assignment2";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.TextBox txtNum1;
+	}
+}
---
+++ Form2.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+	public partial class Form2 : Form
+	{
+		int x;
+		int y;
+		public Form2()
+		{
+			InitializeComponent();
+
+
+		}
+
+		private void panel1_Paint(object sender, PaintEventArgs e)
+		{
+
+		}
+		private void button1_Click(object sender, EventArgs e)
+		{
+			int num = int.Parse(this.txtNum1.Text);
+			for (int i = 0; i < num; i++)
+			{
+				Panel Mypanel1 = new Panel();
+				Mypanel1.Size = new Size(10, 10);
+				Mypanel1.Location = new Point(i * 10, i * 10);
+				Mypanel1.BackColor = Color.Blue;
+				this.panel1.Controls.Add(Mypanel1);
+				//onclick_panel(Mypanel1);
+			}
+			foreach (Control Mypanel in this.panel1.Controls) {
+				Mypanel.MouseDown += Button_MouseDown;
+				Mypanel.MouseUp += Button_MouseUp;
+				Mypanel.MouseMove += Button_MouseMove;
+			}
+		}
+
+		private void Button_MouseDown(object sender, MouseEventArgs e)
+		{
+			x = e.X;
+			y = e.Y;
+			Control c = (Control)sender;
+			c.BackColor = Color.Yellow;
+		}
+
+		private void Button_MouseUp(object sender, MouseEventArgs e)
+		{
+			Control c = (Control)sender;
+			c.BackColor = Color.Blue;
+
+		}
+
+		private void Button_MouseMove(object sender, MouseEventArgs e)
+		{
+			if (e.Button == MouseButtons.Left)
+			{
+				Control c = (Control)sender;
+				c.Location = new Point(e.X + c.Left - x, e.Y + c.Top - y);
+			}
+		}
+
+
+
+
+
+		private void onclick_panel(Panel Mypanel1)
+		{
+			//......
+			Mypanel1.MouseDown += delegate
+				{
+					Mypanel1.BackColor = Color.Yellow;
+				};
+			//Mypanel1.MouseEnter += delegate { Mypanel1.BackColor = Color.Yellow; };
+			//......
+			Mypanel1.MouseUp += delegate
+				{
+					Mypanel1.BackColor = Color.Blue;
+				};
+
+
+		}
+
+		/*private void panel1_MouseUp(object sender, MouseEventArgs e)
+		{
+			Control butt = (Control)sender;
+			//on website if (e.Button == MouseButtons.Left) PanelMouseDownLocation = e.Location;
+		}
+
+		private void panel1_MouseMove(object sender, MouseEventArgs e)
+		{
+			Control c = (Control)sender;
+			c.Location = new Point(e.X + c.Left - x, e.Y + c.Top - y);
+
+
+			//Point p = c.Location;
+			//....On website
+			if (e.Button == MouseButtons.Left)
+
+			{
+
+				panel1.Left += e.X - PanelMouseDownLocation.X;
+
+				panel1.Top += e.Y - PanelMouseDownLocation.Y;
+
+			}*/
+
+	}
+}
